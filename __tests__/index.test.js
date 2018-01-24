1399462c4d025425e8cbf1e51edadf6e989dec84
eithan <eithanshavit@gmail.com>
2018-01-23 22:33:05 -0800

    validateMnemonic

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,28 @@
 
 const shareableSeed = require('../')
 
-test('mnemonicToShares returns 1', () => {
-  expect(shareableSeed.mnemonicToShares()).toBe(1)
+test('validateMnemonic passes', () => {
+  var validMnemonic = 'zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong'
+  expect(shareableSeed.validateMnemonic(validMnemonic)).toBeTruthy()
 })
-  
+
+test('validateMnemonic fails on invalid mnemonic', () => {
+  var invalidMnemonic = 'zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong wrong'
+  expect(() => {
+    shareableSeed.validateMnemonic(invalidMnemonic)
+  }).toThrow(Error('Invalid mnemonic'))
+})
+
+test('validateMnemonic fails on short mnemonic', () => {
+  var invalidMnemonic = 'zoo zoo zoo'
+  expect(() => {
+    shareableSeed.validateMnemonic(invalidMnemonic)
+  }).toThrow(Error('Invalid mnemonic'))
+})
+
+test('validateMnemonic fails on long mnemonic', () => {
+  var invalidMnemonic = 'zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo'
+  expect(() => {
+    shareableSeed.validateMnemonic(invalidMnemonic)
+  }).toThrow(Error('Invalid mnemonic'))
+})
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function mnemonicToShares (mnemonic, threshold, wordlistCode) {
-  return 1
+var bip39 = require('bip39')
+
+const INVALID_MNEMONIC = 'Invalid mnemonic'
+
+function validateMnemonic (mnemonic) {
+  var mnemonicList = mnemonic.trim().split(/\s+/g)
+  if (mnemonicList.count < 12 || mnemonicList.count > 24) throw new Error(INVALID_MNEMONIC)
+  if (!bip39.validateMnemonic(mnemonic)) throw new Error(INVALID_MNEMONIC)
+  return true
 }
 
 module.exports = {
-  mnemonicToShares
+  validateMnemonic
 }
