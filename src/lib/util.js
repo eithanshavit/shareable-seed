5a1327cd6d3efaecf97689ce034939fb4dfa4b02
eithan <eithanshavit@gmail.com>
2018-01-26 21:32:34 -0800

    Math.pow instead of **

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,7 +1,9 @@
 const zeroFill = require('zero-fill')
 
 function numberToHexCode (number, length, isZeroPadded) {
-  const numberInHex = (number % (2 ** (4 * length))).toString(16).slice(0, length)
+  const numberOfBits = 4 * length
+  const trimmedNumber = number % Math.pow(2, numberOfBits)
+  const numberInHex = trimmedNumber.toString(16).slice(0, length)
   if (isZeroPadded) {
     return zeroFill(length, numberInHex)
   }
